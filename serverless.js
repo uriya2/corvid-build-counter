68eaa0d9166bea0be8bf4afd5c99430c3d695b7a
uriyaa <uriyaa@wix.com>
2020-03-22 16:10:48 +0200

    commit my code #serverless

--- serverless.js
+++ serverless.js
@@ -1,5 +1,16 @@
-module.exports = (functionsBuilder) =>
-    functionsBuilder
-        .addWebFunction('GET', '/say-hay', async () => {
-            return `Hello! Today, ${new Date().toLocaleDateString("en-US")}`;
-        });
+const {expect} = require('chai');
+const axios = require('axios');
+const {app} = require('@wix/serverless-testkit');
+
+describe('hello world', () => {
+
+    const scope = 'meow';
+    const testkit = app(scope);
+
+    testkit.beforeAndAfter(10000);
+
+    it('should say hello', async () => {
+        const result = await axios.get(testkit.getUrl('/say-hi'));
+        expect(result.data).to.deep.equal(`Hello! Today is ${new Date().toLocaleDateString("en-US")}`);
+    });
+});
